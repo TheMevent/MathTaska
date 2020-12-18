094d12e86283bacf8e6b7363a11e76e8a39c4744
Mihail <52537051+TheMevent@users.noreply.github.com>
2020-12-18 12:07:43 +0300

    Create determinant.py

---
+++ determinant.py
@@ -0,0 +1,32 @@
+import random
+
+def rowAndIdelete(x, index):
+    new = []
+    for i in range(1, len(x)):
+        a = []
+        for j in range(len(x[0])):
+            if j != index:
+                a.append(x[i][j])
+        new.append(a)
+    return new
+
+
+
+def det(x):
+    if len(x) == 1:
+        return x[0][0]
+    
+    d = 0
+    s = 1
+    for i in range(len(x)):
+        new = rowAndIdelete(x, i)
+        d += s * x[0][i] * det(new)
+        s = -s
+    return d
+
+l = 3
+
+arr = [[random.randint(0, 9) for e in range(3)] for e in range(3)]
+
+print(arr)
+print(det(arr))
